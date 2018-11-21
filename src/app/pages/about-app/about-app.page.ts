a03bfefde51f2130bb22d71c9f225aef93dfe0c4
Krystof Davidek <kry.davidek@gmail.com>
2018-11-21 19:49:41 +0100

    add buttons to about-app page

--- src/app/pages/about-app/about-app.page.ts
+++ src/app/pages/about-app/about-app.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about-app',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutAppPage implements OnInit {
 
-  constructor() { }
+  constructor(public menu: MenuController) { }
 
   ngOnInit() {
+  }
+
+  public openMenu() {
+    this.menu.open();
   }
 
 }
